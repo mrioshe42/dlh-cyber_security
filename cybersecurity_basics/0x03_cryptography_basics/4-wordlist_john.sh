bcdefc7e4b41b1ce3fef2081f0faf6b411221989
mrioshe42 <massimoluigi.rioshernandez@cyber.dlh.lu>
2026-05-14 12:46:10 +0200

    john the ripper

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1"  & john --show --format=raw-md5 "$1" | awk -F: '/^\?:/ {print $2}' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1"
