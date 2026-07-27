34fedee53d2165c504ad732f35ecb5b0962e32db
mrioshe42 <massimoluigi.rioshernandez@cyber.dlh.lu>
2026-07-27 16:29:55 +0200

    fix scripts

--- blue_team/1x04_crypto_foundation/1-symmetric_encrypt.sh
+++ blue_team/1x04_crypto_foundation/1-symmetric_encrypt.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 [[ $# -eq 3 ]] || { echo "Usage: $0 <in_file> <out_file> <cbc|gcm>"; exit 1; }
 [[ -f $1 ]] || { echo "Input file '$1' not found."; exit 1; }
 
-MODE="${3,,}"
+ARG3="$3"
+MODE="${ARG3,,}"
 [[ "$MODE" =~ ^(cbc|gcm)$ ]] || { echo "Mode must be cbc or gcm."; exit 1; }
 
 [[ -z "${MEDDEFENSE_CRYPTO_KEY:-}" ]] && { read -sp "Passphrase: " MEDDEFENSE_CRYPTO_KEY; echo; }
