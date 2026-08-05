b899f7be6fe1c62d910c586a2cfe3d7874cbd229
mrioshe42 <massimoluigi.rioshernandez@cyber.dlh.lu>
2026-08-05 21:49:56 +0200

    fix script

--- blue_team/2x01_windows_fortress/1-domain_findings.ps1
+++ blue_team/2x01_windows_fortress/1-domain_findings.ps1
@@ -144,7 +144,7 @@ try {
     } catch {
         $auditPolConfigured = $false
     }
-    # Process Creation, interactive logon , and Account Management should be enabled for Success auditing
+    # Process Creation, interactive logon , Special Logon , and Account Management should be enabled for Success auditing
     if (-not $auditPolConfigured) {
         $findings.Add([PSCustomObject]@{
             id                      = "FIND-06"
