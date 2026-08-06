a58b3df2d7ec6384750f8b0cc1f0649ace7f6131
mrioshe42 <massimoluigi.rioshernandez@cyber.dlh.lu>
2026-08-06 23:27:07 +0200

    hardened

--- blue_team/2x01_windows_fortress/16-hardened_state_export.ps1
+++ blue_team/2x01_windows_fortress/16-hardened_state_export.ps1
@@ -12,7 +12,7 @@
 .DATE
     Date: 2026-08-06
 .KEYWORDS
-    Evidence Export, JSON, GPO Inventory, Audit Policy, PowerShell Logging, Sysmon, Firewall, AppLocker, RDP, Authentication Protocols, SMB, Service Accounts, Get-ADDomain, Get-GPO, Stop , Start, auditpol 4624
+    Evidence Export, JSON, GPO Inventory, Audit Policy, PowerShell Logging, Sysmon, Firewall, AppLocker, RDP, Authentication Protocols, SMB, Service Accounts, Get-ADDomain, Get-GPO, Stop , Start, auditpol 4624 4625 4688 4672 4697 4698 4720 1102
 .NOTES
     Notes: Requires administrative privileges and RSAT modules. Execute on Domain Controller or Domain-joined Admin Server.
 #>
