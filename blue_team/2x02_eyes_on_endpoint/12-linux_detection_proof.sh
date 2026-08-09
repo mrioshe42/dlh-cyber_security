50a9e8d3078aa2573732e7e901bfb83d25231f2a
mrioshe42 <166904743+mrioshe42@users.noreply.github.com>
2026-08-09 15:26:30 +0200

    Update 12-linux_detection_proof.sh

--- blue_team/2x02_eyes_on_endpoint/12-linux_detection_proof.sh
+++ blue_team/2x02_eyes_on_endpoint/12-linux_detection_proof.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # name: 12-linux_detection_proof.sh
-# purpose: Correlate Linux attack simulation logs against captured telemetry to produce a validation matrix.
+# purpose: Correlate Linux attack simulation logs against captured telemetry to produce a jq validation matrix.
 # author: Massimo Rios
 
 set -e
