7c9eaab8dd323e461b8057853d13b784b1ef501a
mrioshe42 <massimoluigi.rioshernandez@cyber.dlh.lu>
2026-08-20 09:49:10 +0200

    perimeter defense

--- blue_team/2x04_perimeter_defense/13-dns_filtering.sh
+++ blue_team/2x04_perimeter_defense/13-dns_filtering.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script Name: 13-dns_filtering.sh
 # Description: Configures local DNS filtering via dnsmasq (sinkholing blocklists,
-#              forwarding valid queries, logging queries) and validates resolution paths.
+#              forwarding valid queries, logging queries) and validates resolution paths. jq
 # Author: Massimo
 
 set -uo pipefail
