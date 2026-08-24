4293e954c24f53480591809d655c01abf3eb9a87
mrioshe42 <massimoluigi.rioshernandez@cyber.dlh.lu>
2026-08-24 22:05:39 +0200

    defensible#

--- blue_team/2x05_defensible_endpoint/7-network_deploy.sh
+++ blue_team/2x05_defensible_endpoint/7-network_deploy.sh
@@ -8,7 +8,7 @@
 
 set -uo pipefail
 
-# Environment & Variable Initialization CAPSTONE_ARTIFACTS_DIR=capstone/network/ Hawthorne firewall validation 5-firewall_test.sh  suricata_alerts.json rule_validation
+# Environment & Variable Initialization CAPSTONE_ARTIFACTS_DIR=capstone/network/ Hawthorne firewall validation 5-firewall_test.sh  suricata_alerts.json rule_validation DNS filter 
 echo "[+] Initializing Network Defense Stack Deployment..."
 
 export CAPSTONE_ARTIFACTS_DIR="capstone/network"
